74a47a0cad3f0629f60118a9d61cb98bd92f482d
wiserenn <akincilareren@gmail.com>
2022-10-27 02:08:26 +0300

    modified:   src/components/Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>klfsh</div>;
+  return <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" href="mailto:csc@atu.edu.tr">Contact US</a>;
 };
 
 export default Contact;
